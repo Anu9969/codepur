2b8c572666a37364e05b79a3e14e8db1176c10e8
Anu9969 <sharmaanurag9969@gmail.com>
2024-07-10 01:00:38 +0530

    added Form and validators

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,78 +9,102 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool ChangeBtn = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        ChangeBtn = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        ChangeBtn = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.network(
-              "https://via.placeholder.com/150", // Placeholder image URL
-              fit: BoxFit.cover,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Welcome $name',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter Username',
-                      labelText: 'Username',
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        name = value;
-                      });
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Enter Password',
-                      labelText: 'Password',
-                    ),
-                  ),
-                ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.network(
+                "https://via.placeholder.com/150", // Placeholder image URL
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(height: 20),
-            Material(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(8),
-              child: InkWell(
-                onTap: () async {
-                  setState(() {
-                    ChangeBtn = true;
-                  });
-                  await Future.delayed(const Duration(seconds: 1));
-                  await Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  setState(() {
-                    ChangeBtn = false;
-                  });
-                },
-                child: AnimatedContainer(
-                  duration: Duration(seconds: 1),
-                  width: ChangeBtn ? 50 : 150,
-                  height: 50,
-                  alignment: Alignment.center,
-                  child: ChangeBtn
-                      ? Icon(Icons.done)
-                      : Text(
-                          "Login",
-                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome $name',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter Username',
+                        labelText: 'Username',
+                      ),
+                      validator: (value){
+                        if(value?.isEmpty ?? true){     //samjh nhi aaya ye thoda
+                          return "Username cannot be empty";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Password',
+                        labelText: 'Password',
+                      ),
+                    validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password cannot be empty";
+                        } else if (value.length < 6) {
+                          return "Password length should be at least 6";
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Material(
+                color:ChangeBtn?Colors.green: Colors.blue,
+                borderRadius: ChangeBtn?BorderRadius.circular(30):BorderRadius.circular(8),
+                child: InkWell(
+                  onTap: () => moveToHome(context),
+                
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    width: ChangeBtn ? 50 : 150,
+                    height: 50,
+                   
+                    alignment: Alignment.center,
+                    child: ChangeBtn
+                        ? Icon(Icons.done)
+                        : Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
