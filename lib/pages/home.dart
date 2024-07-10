258d05307cfdbd31a5c7696aeaf206e73b3e2aca
Anu9969 <sharmaanurag9969@gmail.com>
2024-07-11 00:44:35 +0530

    used catalog.json isliye changed stateless to stateful,used initstate,used decoder for json

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,23 +1,42 @@
+import 'dart:convert';
+
 import 'package:codepur/models/catalog.dart';
 import 'package:flutter/material.dart';
 import 'package:codepur/widgets/drawer.dart';
 import 'package:codepur/widgets/item_widgets.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 4;
+
   final String name = "Let's Upgrade";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();  
+    loadData();
+      //build hone ke pehle ye call hoga to get the data from the json pehle hi
+  }
+
+  loadData() async{
+    final Json = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedJson = jsonDecode(Json);
+    var productData = decodedJson["items"];
+    CatalogModel.items = List.from(productData)
+        .map<Item>((item) => Item.fromMap(item)).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Catalog App', textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold),)),
-        // leading: IconButton(
-        //   icon: Icon(Icons.arrow_back),
-        //   onPressed: () {
-        //     Navigator.pop(context);
-        //   },
-        // ),
       ),
       body:Padding(
         padding: const EdgeInsets.all(16.0),
